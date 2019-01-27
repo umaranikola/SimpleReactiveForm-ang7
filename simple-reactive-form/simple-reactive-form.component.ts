693f34bc80c997cdd7461686653bce2fc4c5342e
umaranikola <38150516+umaranikola@users.noreply.github.com>
2019-01-27 18:04:24 +0530

    Update simple-reactive-form.component.ts

--- simple-reactive-form/simple-reactive-form.component.ts
+++ simple-reactive-form/simple-reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// Import FormGroup and FormControl classes
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -7,9 +8,12 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./simple-reactive-form.component.css']
 })
 export class SimpleReactiveFormComponent implements OnInit {
+   // This FormGroup contains fullName and Email form controls
   employeeForm:FormGroup;
   constructor() { }
-
+  // Initialise the FormGroup with the 2 FormControls we need.
+  // ngOnInit ensures the FormGroup and it's form controls are
+  // created when the component is initialised
   ngOnInit() {
     this.employeeForm= new FormGroup({
       fullName: new FormControl(),

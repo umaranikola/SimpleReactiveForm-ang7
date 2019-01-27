1dcdfd0bd6b462eace09a8629851a44c29265d6a
umaranikola <38150516+umaranikola@users.noreply.github.com>
2019-01-27 18:02:27 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,26 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { ListEmployeesComponent } from './employees/list-employees.component';
-import { AddressDataComponent } from './address-data/address-data.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { StudentsModule } from './students/students.module';
-import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
-import { EmployeeService } from './employee.service';
-import { CreateEmpoyeeComponent } from './create-empoyee/create-empoyee.component';
 import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 import { SimpleReactiveFormComponent } from './simple-reactive-form/simple-reactive-form.component';
 
 @NgModule({
   declarations: [ //goes all your components  (e.g., AppComponent, TodoComponent)
     AppComponent,
-    TestComponent,
-    ListEmployeesComponent,
-    AddressDataComponent,
-    EmployeeDetailsComponent,
-    CreateEmpoyeeComponent,
     ReactiveformComponent,
     SimpleReactiveFormComponent,
    
@@ -28,7 +15,6 @@ import { SimpleReactiveFormComponent } from './simple-reactive-form/simple-react
   imports: [ // Imports: routes and modules go here.
     BrowserModule ,
     FormsModule,
-    StudentsModule,
     ReactiveFormsModule
   ],
   providers: [EmployeeService],
